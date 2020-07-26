108194a6731cf241fe1f8a9897c69356d168b486
Sarabjeet Singh <sarabjeet303@gmail.com>
2020-07-26 20:14:34 +0000

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+var request = require('supertest');
+var express = require('express');
+ 
+var app = require('../app');
+ 
+describe('Index Page', function() {
+  it("renders successfully", function(done) {
+    request(app).get('/').expect(200, done);    
+  })
+})
